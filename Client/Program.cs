80534c9725aa09d5eb08331f57d326c57436d71e
Naroci <mircosgiga@hotmail.de>
2025-07-23 12:02:00 +0900

    Removed the Browser project because it does not support sockets.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Sockets;
 using System.Text;
+using Connection.Shared;
 using Connection.Shared.Connection.Client;
 
 namespace Client;
@@ -16,13 +17,7 @@ class Program
         ClientConnection connection = new ClientConnection();
         connection.SetReconnectAttempts(10);
         connection.Connect("localhost", 5555);
-        connection.OnPackageReceived += package =>
-        {
-            Console.WriteLine($"[{package.GetTimestamp().TimeOfDay}, {package.GetId()}] Received: " +
-                              $"\n ID : {package.GetShortId()} (Short)" +
-                              $"\n Size : {package.GetContentSize()}" +
-                              $"\n Content : [{package.GetContentAsString()}]");
-        };
+        connection.OnPackageReceived += OnPackageReceived;
         while (running)
         {
             var message = Console.ReadLine();
@@ -37,7 +32,14 @@ class Program
                 Console.WriteLine("Not Connected.");
             }
         }
-
         Console.WriteLine("Ended.");
+    }
+
+    static void OnPackageReceived(ConnectionPackage package)
+    {
+        Console.WriteLine($"[{package.GetTimestamp().TimeOfDay}, {package.GetId()}] Received: " +
+                          $"\n ID : {package.GetShortId()} (Short)" +
+                          $"\n Size : {package.GetContentSize()}" +
+                          $"\n Content : [{package.GetContentAsString()}]");
     }
 }
